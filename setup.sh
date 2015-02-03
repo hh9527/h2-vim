3479c4f8564ef7582f2ff1e5236ed1945479b913
huhao <hh9527@gmail.com>
2015-02-03 14:02:30 +0800

    bugfix

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-if [ ! -e ~/.vim/bundle ];
+if [ ! -e ~/.vim/bundle ]; then
   mkdir -p ~/.vim/bundle
 fi
-if [ ! -e ~/.vim/bundle/Vundle.vim ];
+if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-if [ ! -e ~/.vim/bundle/h2-vim ];
+if [ ! -e ~/.vim/bundle/h2-vim ]; then
   git clone https://github.com/hh9527/h2-vim.git ~/.vim/bundle/h2-vim
 fi
 
